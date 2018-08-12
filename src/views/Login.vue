b725c24c7c75a09429129108cc3c533b31b1b486
Elives <eliveshuang1992gmail.com>
2018-08-13 00:31:32 +0800

    登录页完善

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="wrap flex">
-    	<!-- <van-cell-group>
+        <!-- <van-cell-group>
 		  <van-field
 		    v-model="username"
 		    label="用户名"
@@ -14,84 +14,119 @@
 		    placeholder="请输入密码"
 		  />
 		</van-cell-group> -->
-		<div class="welcom-top">
-			WELCOME
-		</div>
-		<div class="welcom">
-			欢迎来到紫藤农业
-		</div>
-		<div class="login-area">
-			<div class="user-name">
-				<div>
-					账户
-				</div>
-				<van-cell-group>
-					<van-field
-		    v-model="username"
-		    placeholder="请输入用户名"
-		  />
-				</van-cell-group>
-				
-			</div>
-		</div>
+        <div class="welcom-top">
+            WELCOME
+        </div>
+        <div class="welcom">
+            欢迎来到紫藤农业
+        </div>
+        <div class="login-area">
+            <div>
+                账户
+            </div>
+            <van-field v-model="username" placeholder="请输入用户名" />
+            <div class="pass-txt">
+                密码
+            </div>
+            <van-field v-model="password" class="pass-input" type="password" placeholder="请输入密码" />
+        </div>
+        <div class="forget flex">
+        	忘记密码？
+        </div>
+        <div class="flex login-btn-bottom">
+        	<van-button type="primary" class="login-btn">登录</van-button>
+        </div>
     </div>
 </template>
-
 <script>
-	import { Field,CellGroup } from 'vant';
-    export default {
-        name: "Login",
-        data(){
-        	return{
-        		username:"",
-        		password:""
-        	}
-        },
-        components: {
-            Field,
-            [CellGroup.name]:CellGroup,
-            [Field.name]:Field,
-        },
-    }
+import { Field, CellGroup,Button } from 'vant';
+export default {
+    name: "Login",
+    data() {
+        return {
+            username: "",
+            password: ""
+        }
+    },
+    components: {
+        Field,
+        [CellGroup.name]: CellGroup,
+        [Field.name]: Field,
+        [Button.name]: Button,
+    },
+}
 </script>
-
 <style scoped>
-.flex{
-	display:flex;
-	justify-content:flex-start;
-	align-items:center;
-	flex-wrap:wrap;
+.flex {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    flex-wrap: wrap;
 }
-.wrap{
-	padding:0 0.8rem;
+
+.wrap {
+    padding: 0 0.8rem;
 }
-.welcom-top{
-	margin-top:1.48rem;
-	font-size: 0.32rem;
-	line-height:0.44rem;
-	color: #202323;
+
+.welcom-top {
+    margin-top: 1.48rem;
+    font-size: 0.32rem;
+    line-height: 0.44rem;
+    color: #202323;
+    width: 100%;
+    text-align: left;
+}
+
+.welcom {
+    font-size: 0.64rem;
+    line-height: 0.893333rem;
+    color: #202323;
+    margin: 0.106667rem 0 0.933333rem 0;
+    width: 100%;
+    text-align: left;
+}
+
+.login-area {
+    height: 5.653333rem;
+    background: #FFFFFF;
+    box-shadow: 0 0.16rem 0.32rem 0 rgba(84, 160, 156, 0.09);
+    border-radius: 0.213333rem;
+    padding: 0.746667rem 0.72rem 0;
+    width: 100%;
+    text-align: left;
+    font-size: 0.373333rem;
+	color: #777777;
+}
+
+.pass-txt{
+	margin-top:0.986667rem;
+}
+.van-cell::after {
+    border-bottom-width: 0.026667rem !important;
+    left: 0.4rem;
+    right: 0;
+    width: auto;
+    -webkit-transform: scale(1,.5);
+    transform: scale(1,.5);
+}
+.forget{
+	font-size: 0.426667rem;
+	color: #B7C1CB;
+	justify-content:flex-end;
 	width:100%;
-	text-align:left;
+	margin-top:0.373333rem;
 }
-.welcom{
-	font-size: 0.64rem;
-	line-height:0.893333rem;
-	color: #202323;
-	margin:0.106667rem 0 0.933333rem 0;
+.login-btn-bottom{
 	width:100%;
-	text-align:left;
+	justify-content:center;
+	margin-top:1.466667rem;
 }
-.login-area{
-	height:5.653333rem;
-	background: #FFFFFF;
-	box-shadow: 0 0.16rem 0.32rem 0 rgba(84,160,156,0.09);
-	border-radius: 0.213333rem;
-	padding:0.746667rem 0.72rem 0;
-	width:100%;
-	text-align:left;
-}
-.user-name{
-	font-size: 0.373333rem;
-	margin-bottom:0.213333rem;
+.login-btn{
+	width:6.4rem;
+	height:1.173333rem;
+	border-radius:1.173333rem;
+	background:#44e3a8;
+	font-size: 0.426667rem;
+	border:none;
 }
 </style>
