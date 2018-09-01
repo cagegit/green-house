c936e68f7db2016978c9a7bec26bf8019c904289
cagejet <cagejet@gmail.com>
2018-09-01 10:46:32 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-    baseUrl: '.', // 根域上下文目录
+    baseUrl: '/', // 根域上下文目录
     outputDir: 'dist', // 构建输出目录
     assetsDir: 'static', // 静态资源目录 (js, css, img, fonts)
     lintOnSave: false, // 是否开启eslint保存检测，有效值：ture | false | 'error'
